81ff6dfbff839392a480b013104ef6862c953623
anastasiiaut <124287939+anastasiiaut@users.noreply.github.com>
2024-03-25 22:34:57 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,9 @@
 #include "fun.h"
 
 int main() {
+    const char *str = "This is a 123 string With 456numbers 789";
+    std::cout << "1: " << faStr1(str) << std::endl;
+    std::cout << "2: " << faStr2(str) << std::endl;
+    std::cout << "3: " << faStr3(str) << std::endl;
     return 0;
 }
