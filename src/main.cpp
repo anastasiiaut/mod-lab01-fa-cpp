f4a883d679f94652cce118a89e27ded8d15ab20f
anastasiiaut <124287939+anastasiiaut@users.noreply.github.com>
2024-03-25 19:20:56 +0300

    Данцева А. 382007-3

--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,11 @@
 #include "fun.h"
 
 int main() {
+    const char* str = "Hello World 123 I love programming";
+
+    std::cout << "Количество слов в строке, которые не содержат цифры: " << faStr1(str) << std::endl;
+    std::cout << "Количество слов в строке, которые начинаются с заглавной буквы и содержат только строчные буквы и пробелы: " << faStr2(str) << std::endl;
+    std::cout << "Средняя длина слов в строке: " << faStr3(str) << std::endl;
+
     return 0;
 }
