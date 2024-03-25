6946da9073ee26e94b54bd06553ec443f40acfeb
anastasiiaut <124287939+anastasiiaut@users.noreply.github.com>
2024-03-25 19:20:11 +0300

    Данцева А. 382007-3

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,75 +1,87 @@
-// Copyright 2022 UNN-IASR
-#include "fun.h"
 #include <iostream>
 #include <cstring>
 #include <cctype>
+#include <sstream>
 
-unsigned int faStr1(const char *str) {
- unsigned int count = 0;
-	char* strCopy = new char[strlen(str) + 1];
-	strcpy(strCopy, str);
+unsigned int faStr1(const char* str) 
+{
+    unsigned int count = 0;
+    char* strCopy = new char[strlen(str) + 1];
+    strcpy(strCopy, str);
 
-	char* word = strtok(strCopy, " ");
+    char* word = strtok(strCopy, " ");
 
-	while (word != NULL) {
-		bool valid = true;
-		for (size_t i = 0; i < strlen(word); i++) {
-			if (isdigit(word[i])) {
-				valid = false;
-				break;
-			}
-		}
+    while (word != NULL) 
+    {
+        bool valid = true;
+        for (size_t i = 0; i < strlen(word); i++) 
+        {
+            if (isdigit(word[i])) 
+            {
+                valid = false;
+                break;
+            }
+        }
 
-		if (valid) {
-			count++;
-		}
+        if (valid) 
+        {
+            count++;
+        }
 
-		word = strtok(NULL, " ");
-	}
+        word = strtok(NULL, " ");
+    }
 
-	delete[] strCopy;
-	return count;
+    delete[] strCopy;
+    return count;
 }
 
-unsigned int faStr2(const char *str) {
-unsigned int count = 0;
-	char* strCopy = new char[strlen(str) + 1];
-	strcpy(strCopy, str);
+unsigned int faStr2(const char* str) 
+{
+    unsigned int count = 0;
+    char* strCopy = new char[strlen(str) + 1];
+    strcpy(strCopy, str);
 
-	char* word = strtok(strCopy, " ");
+    char* word = strtok(strCopy, " ");
 
-	while (word != NULL) {
-		if (isupper(word[0])) {
-			bool valid = true;
-			for (size_t i = 1; i < strlen(word); i++) {
-				if (!islower(word[i]) && !isspace(word[i])) {
-					valid = false;
-					break;
-				}
-			}
+    while (word != NULL) 
+    {
+        if (isupper(word[0])) 
+        {
+            bool valid = true;
+            for (size_t i = 1; i < strlen(word); i++) 
+            {
+                if (!islower(word[i]) && !isspace(word[i])) 
+                {
+                    valid = false;
+                    break;
+                }
+            }
 
-			if (valid) {
-				count++;
-			}
-		}
+            if (valid) 
+            {
+                count++;
+            }
+        }
 
-		word = strtok(NULL, " ");
-	}
+        word = strtok(NULL, " ");
+    }
 
-	delete[] strCopy;
-	return count;
+    delete[] strCopy;
+    return count;
 }
 
-unsigned int faStr3(const char *str) {
-unsigned int wordCount = 0;
-	unsigned int totalLength = 0;
-	std::istringstream iss(str);
-	std::string word;
+unsigned int faStr3(const char* str) 
+{
+    unsigned int wordCount = 0;
+    unsigned int totalLength = 0;
+    std::istringstream iss(str);
+    std::string word;
 
-	while (iss >> word) {
-		wordCount++;
-		totalLength += word.length();
-	}
+    while (iss >> word) 
+    {
+        wordCount++;
+        totalLength += word.length();
+    }
 
-	return wordCount == 0 ? 0 : totalLength / wordCount;
+    return wordCount == 0 ? 0 : totalLength / wordCount;
 }
